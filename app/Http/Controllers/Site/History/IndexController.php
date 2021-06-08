40d973e0dd59d91d453771ef0d4b34da999c77e0
Denis Bedoyar <pulpirzvk@gmail.com>
2021-06-08 13:42:13 +0700

    Структура страниц
    - контроллеры
    - роуты
    - вьюхи
    - мастер-шаблоны

---
+++ app/Http/Controllers/Site/History/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Site\History;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return Application|Factory|View
+     */
+    public function __invoke(Request $request): View|Factory|Application
+    {
+        $data = [];
+
+        return view('site.history.index', $data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', Site\IndexController::class)->name('site.index');
+
+Route::get('/news', Site\News\IndexController::class)->name('site.news.index');
+Route::get('/news/{newsItem}', Site\News\ItemController::class)->name('site.news.item');
+
+Route::get('/history', Site\History\IndexController::class)->name('site.history.index');
+Route::get('/history/{historyItem}', Site\History\ItemController::class)->name('site.history.item');
+
+Route::get('/about', Site\AboutController::class)->name('site.about.index');
+Route::get('/contacts', Site\ContactsController::class)->name('site.contacts.index');
+Route::get('/customer', Site\CustomerController::class)->name('site.customer.index');
+Route::get('/form', Site\FormController::class)->name('site.form.index');
+Route::get('/job', Site\JobController::class)->name('site.job.index');
+Route::get('/landing', Site\LandingController::class)->name('site.landing.index');
+Route::get('/oracle', Site\OracleController::class)->name('site.oracle.index');
