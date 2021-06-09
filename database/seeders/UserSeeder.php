3bdfb70e40a8d2cdc38845fa7e59c5535a194303
Denis Bedoyar <pulpirzvk@gmail.com>
2021-06-10 01:36:10 +0700

    Создание тестового пользователя админки

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,9 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory(1)->create([
+            'email' => 'admin@getcrm.ru',
+        ]);
+        User::factory(1)->create([
             'email' => 'user@getcrm.ru',
         ]);
     }
