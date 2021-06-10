ad60d7035fc773c6df26feff64c4a9c32c627202
Denis Bedoyar <pulpirzvk@gmail.com>
2021-06-10 17:15:24 +0700

    Доработка блока "Вакансии" на главной

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1457,57 +1457,7 @@
             </div>
         </div>
     </div>
-    <div class="vacancies_bk">
-        <div class="container">
-            <div class="title_bk">Мы сделаны из того же теста!</div>
-            <div class="vacancies_wrapper_inside">
-                <div class="vacancies_inside d_flex a_items_center">
-                    <div class="vacancies_image">
-                        <img src="/img/temp/vacancies_1.jpg" alt=""/>
-                    </div>
-                    <div class="vacancies_info d_flex a_items_center">
-                        <div class="vacancies_info_inside">
-                            <div class="vacancies_info_title">
-                                Наша замечательная команда
-                            </div>
-                            <div class="vacancies_info_desc">
-                                <p>
-                                    Если вы готовы менять мир к лучшему, знаете как улучшить
-                                    опыт покупаетеля на пути покупки необходимого ему товара.
-                                </p>
-                                <p>
-                                    Дале ещё более вдохновляющий текст по мотивации будующих
-                                    сотрудников которые станут новой кровью нашей компании.
-                                </p>
-                            </div>
-                            <div class="vacancies_info_btn">
-                                <a href="#" class="btn btn_orange btn_orange_with_border">
-                                    <span>Вакансии</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="vacancies_btn">
-                    <div class="vacancies_btn--wrapper">
-                        <a
-                            href="#"
-                            class="vacancies_btn_link d_flex a_items_center j_content_center"
-                        >
-                  <span class="vacancies_btn_link--icon">
-                    <img src="/img/vacancies_hh_icon.png" alt=""/>
-                  </span>
-                            <span class="vacancies_btn_link--txt">
-                    <span class="vacancies_btn_link--txt_one"
-                    >Профиль нашей компании
-                    </span>
-                    <span class="vacancies_btn_link--txt_two">на hh.ru</span>
-                  </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    @include('_partials.home.job')
     @include('_partials.home.news')
 @endsection
