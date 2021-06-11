1d725aaef8158408f3490de248aec0dd10f79ec3
Denis Bedoyar <pulpirzvk@gmail.com>
2021-06-11 17:33:23 +0700

    Генерация даты создания новости в диапазоне 2018-2021 для тестовых новостей.

--- database/seeders/NewsItemSeeder.php
+++ database/seeders/NewsItemSeeder.php
@@ -3,12 +3,31 @@
 namespace Database\Seeders;
 
 use App\Models\NewsItem;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class NewsItemSeeder extends Seeder
 {
     public function run(): void
     {
-        NewsItem::factory(10)->create();
+        /** @var Collection|NewsItem[] $news */
+        $news = NewsItem::factory(20)->create();
+
+        $startYear = 2018;
+
+        foreach ($news->chunk(3) as $key => $newsChunk) {
+            $currentYear = $startYear + $key;
+
+            /** @var Collection|NewsItem[] $newsChunk */
+            $newsChunk->each(static function (NewsItem $item) use ($currentYear) {
+                $item->update([
+                    'created_at' => $item->created_at->setYear($currentYear),
+                ]);
+            });
+
+            if ($currentYear === 2020) {
+                break;
+            }
+        }
     }
 }
