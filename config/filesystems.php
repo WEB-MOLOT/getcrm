1626ce1dd3693064d3556e5829e5652f0ca58b41
Denis Bedoyar <pulpirzvk@gmail.com>
2021-07-07 19:13:45 +0700

    Перенос данных - вакансии

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,10 +35,15 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'storage' => [
+            'driver' => 'local',
+            'root' => storage_path(),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
--- database/seeders/VacancySeeder.php
+++ database/seeders/VacancySeeder.php
@@ -3,12 +3,33 @@
 namespace Database\Seeders;
 
 use App\Models\Vacancy;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class VacancySeeder extends Seeder
 {
+    /**
+     * @throws FileNotFoundException
+     */
     public function run(): void
     {
-        Vacancy::factory(5)->create();
+        foreach ($this->vacancies as $vacancy) {
+            $vacancy['content'] = Storage::disk('storage')->get($vacancy['content']);
+            Vacancy::factory()->create($vacancy);
+        }
     }
+
+    protected array $vacancies = [
+        [
+            'title' => 'Бизнес-консультант ИТ-проектов',
+            'content' => 'data/vacancies/1.txt',
+            'hh' => 'https://hh.ru/vacancy/45232010',
+            'params' => [
+                'salary' => '50 000 - 200 000 на руки',
+                'experience' => '3-6 лет',
+                'employment' => 'Полная занятость, полный день',
+            ],
+        ],
+    ];
 }
