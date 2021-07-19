b247576ac63816622a26559f25debcbdc4d9c689
Denis Bedoyar <pulpirzvk@gmail.com>
2021-07-19 22:48:40 +0700

    Мелкие правки

--- app/Http/Controllers/Cabinet/IndexController.php
+++ app/Http/Controllers/Cabinet/IndexController.php
@@ -4,18 +4,23 @@ namespace App\Http\Controllers\Cabinet;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function __invoke(Request $request)
+    /**
+     * Главная страница ЛК
+     *
+     * @param Request $request
+     * @return Application|Factory|View
+     */
+    public function __invoke(Request $request): View|Factory|Application
     {
         /** @var User $user */
         $user = $request->user();
-
-//        if ($user->isAdmin()) {
-//            return response()->redirectTo('/admin');
-//        }
 
         $data = [
             'hasProductMark' => $user->products()->active()->mark()->exists(),
