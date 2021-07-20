b3afb4ae5159accdf53e87e5971bf44c5d9b55d2
Denis Bedoyar <pulpirzvk@gmail.com>
2021-07-20 14:50:34 +0700

    Фикс ошибки развертывания приложения

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,30 +5,23 @@ namespace App\Providers;
 use App\Enums\SettingType;
 use App\Models\Setting;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         Paginator::defaultView('_partials.paginator');
 
-        $this->getConfigFromDb();
+        if (Schema::hasTable(Setting::getModel()->getTable())) {
+            $this->getConfigFromDb();
+        }
     }
 
     protected function getConfigFromDb()
