51edd8f6a2f24a3ca58b529d6e6d3f3ee2685110
Denis Bedoyar <pulpirzvk@gmail.com>
2021-07-21 16:19:57 +0700

    Закрыть доступ для неактивных пользователей

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -23,21 +23,28 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request): bool
+    {
+        $credentials = $this->credentials($request);
+
+        $credentials['is_active'] = 1;
+
+        return $this->guard()->attempt(
+            $credentials, $request->filled('remember')
+        );
     }
 
     /**
