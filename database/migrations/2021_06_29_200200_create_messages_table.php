7b618c5496128c8d90bdc5e503a0ca39f3f2ab73
Denis Bedoyar <pulpirzvk@gmail.com>
2021-08-09 11:38:38 +0700

    Чистка миграций, моделей, фабрик

--- database/migrations/2021_06_29_200200_create_messages_table.php
+++ database/migrations/2021_06_29_200200_create_messages_table.php
@@ -6,12 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateMessagesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
@@ -20,12 +15,7 @@ class CreateMessagesTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('messages');
     }
